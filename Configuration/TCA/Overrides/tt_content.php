30657acf0b82215525b3b8c07d41b52a33f4a243
Sebastian During <colaholiker@nixos1.svbalingen.de>
2024-08-14 14:16:21 +0200

    asdf

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -12,8 +12,8 @@ call_user_func(
             'viewtournament'
         );
 
-        #$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['svbschachturniere_viewtournament'] = 'pi_flexform';
-        #\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('svbschachturniere_viewtournament', 'FILE:EXT:svb_schachturniere/Configuration/FlexForm/flexform_schachturniere.xml');
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['svbschachturniere_viewtournament'] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('svbschachturniere_viewtournament', 'FILE:EXT:svb_schachturniere/Configuration/FlexForm/flexform_schachturniere.xml');
     },
 );
 
